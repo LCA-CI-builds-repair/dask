7dcb691033b6f208b5143ae06b32d5a347819e30
ubuntu <ubuntu@ip-10-0-1-10>
2025-01-29 16:50:29 +0000

    waleko__claude-3-opus_152a__id_140

--- dask/utils.py
+++ dask/utils.py
@@ -250,7 +250,7 @@ def _deprecated_kwarg(
                 else:
                     new_arg_value = old_arg_value
                     msg = (
-                        f"the {repr(old_arg_name)} keyword is deprecated, "
+                        f"the {old_arg_name!r} keyword is deprecated, "
                         f"use {repr(new_arg_name)} instead."
                     )
 
