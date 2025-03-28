f975a78ad228ea06b88e25c51ddcbd2cb2cd1247
ubuntu <ubuntu@ip-10-0-0-64>
2025-03-28 11:35:26 +0000

    waleko__o1_12b0__id_140

--- dask/utils.py
+++ dask/utils.py
@@ -148,7 +148,7 @@ def _deprecated_kwarg(
     new_arg_name: str | None = None,
     mapping: Mapping[Any, Any] | Callable[[Any], Any] | None = None,
     stacklevel: int = 2,
-    comment: str | None = None
+    comment: str | None = None,
 ) -> Callable[[F], F]:
     """
     Decorator to deprecate a keyword argument of a function.
